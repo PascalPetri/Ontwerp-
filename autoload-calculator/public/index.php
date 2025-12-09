8d0bba15e181b40637f80d4db26f38dd865213ac
PascalPetri <9018608@student.zadkine.nl>
2025-12-09 10:18:01 +0100

    Autoloader Calculator
    v2

--- autoload-calculator/public/index.php
+++ autoload-calculator/public/index.php
@@ -1,23 +1,33 @@
 <?php
-// AUTOLOADER INCLUDEN
-require __DIR__ . '/../vendor/autoload.php';
-
+require '../vendor/autoload.php';
 use Autoload\Calculator\Calculator;
 
-$input3 = '';
-if (isset($_POST['input1']) && isset($_POST['input2'])) {
+$cijfer3 = '';
+if ($_POST && !empty($_POST['cijfer1']) && !empty($_POST['cijfer2'])) {
     $calc = new Calculator();
-    $input3 = $calc->maakInput3($_POST['input1'], $_POST['input2']);
+    $cijfer3 = $calc->maakInput3($_POST['cijfer1'], $_POST['cijfer2']);
 }
 ?>
 
-<h1>Autoload Calculator</h1>
-<form method="post">
-    Input 1: <input name="input1"><br>
-    Input 2: <input name="input2"><br>
-    <input type="submit" value="Maak Input 3">
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Autoloader Calculator</title>
+</head>
+<body>
+    <h1>Autoloader Calculator</h1>
+    
+    <form method="post">
+        Cijfer 1: <input type="number" name="cijfer1" required><br><br>
+        Cijfer 2: <input type="number" name="cijfer2" required><br><br>
+        <button type="submit">Bereken Cijfer 3</button>
+    </form>
 
-<?php if ($input3 !== ''): ?>
-    <h2>Input 3 = <?php echo $input3; ?></h2>
-<?php endif; ?>
+    <?php if ($cijfer3 !== ''): ?>
+        <h2>Resultaat</h2>
+        <p><strong>Cijfer 3 = <?php echo $cijfer3; ?></strong></p>
+    <?php elseif ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+        <p style="color:red">Vul beide cijfers in!</p>
+    <?php endif; ?>
+</body>
+</html>
